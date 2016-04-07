c6d22324729ee8456a124b0fea5d3883428a1df1
anastasiia.shypko <anastasiia.shypko@netent.com>
2016-04-07 17:57:30 +0300

    winbox

--- js/spinButton.js
+++ js/spinButton.js
@@ -1,13 +1,13 @@
-function SButton(x, y, url){
+function SButton(x, y, url) {
     var me = this;
 
-    this.x= x;
+    this.x = x;
     this.y = y;
     this.path = url;
     this.state = 'up';
 
     //метод инит, будет принемать контейнер, будет создавать спрайт, задавать позицию
-    this.init = function(mainStage){
+    this.init = function (mainStage) {
 
         var newButton = PIXI.Sprite.fromImage(me.path);
         newButton.position.set(me.x, me.y);
@@ -25,35 +25,57 @@ function SButton(x, y, url){
 
     };
 
-    this.setState = function(){
+    this.setState = function () {
 
     };
 
-    this.onClickCallBack = function(){
+    this.onClickCallBack = function () {
 
     };
 
-    this.onClick = function (){
-        if (me.state !== 'down'){
+    this.update = function () {
+        if (me.state == 'down') {
+            me.path = 'image/buttonDown.png';
+        }
+
+        if (me.state == 'hover') {
+            me.height = me.height*1.5;
+
+        }
+
+    };
+
+    this.onClick = function () {
+        if (me.state !== 'down') {
             me.state = 'down';
             fireEvent('buttonPressed');
-            console.log(me.state)
-            newButton = PIXI.Sprite.fromImage(me.path);
-        };
-    };
-
-    this.onHover = function (state) {
-    }
-
-    this.onClickUp = function (state){
-
-    };
-
-    this.onDisabled = function (state){
-        if (state == 'down'){
-            console.log('true!!!')
+            me.update();
         }
     };
+
+    this.onHover = function () {
+        if (me.state !== 'hover') {
+            me.state = 'hover';
+            me.update();
+        }
+    };
+
+    this.onClickUp = function () {
+        if (me.state !== 'up') {
+            me.state = 'up';
+            me.update();
+        }
+    };
+
+    this.onDisabled = function (state) {
+        if (state == 'down') {
+            console.log('true!!!')
+        }
+    addListener('allReelsStopped', me.onClickUp)
+
+    };
+
+
 
 
 
